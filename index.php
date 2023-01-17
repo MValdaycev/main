aaf57e6111632ec7ab2e2140502f09c1b2ba084e
MaximValdaycev <mvaldaycev@gmail.com>
2023-01-17 15:18:10 +0500

    test

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width", initial-scale=1.0>
+        <title>website</title>
+        <p>Первая Страница</p>
+        <p>2<a href="second.php"></a></p>
+    </head>
+    <body>        
+    </body>
+</html>
