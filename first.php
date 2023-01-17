48bf6c066df8e7d89b78b19c28196eea5e6a5072
MaximValdaycev <mvaldaycev@gmail.com>
2023-01-17 15:23:06 +0500

    test

--- first.php
+++ first.php
@@ -5,7 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width", initial-scale=1.0>
         <title>website</title>
-        <p>Первая Страница</p>
+        <p>1 Страница</p>
+        <p>index<a href="index.html"></a></p>
+        <p>1<a href="first.php"></a></p>
         <p>2<a href="second.php"></a></p>
     </head>
     <body>        
